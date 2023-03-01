bff079d30eb59a670615dd7f9dfdc5efa43ce602
Wandonium <hillarywando@gmail.com>
2023-03-01 16:04:58 +0300

    Deleted all components and unnecessary files. Starting from scratch.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
-import './App.css';
-import Navigation  from './components/Navigation/Navigation'
-import Showcase from './components/Showcase/Showcase'
-import Testimonial from './components/Testimonials/testimonial'
-import Services from './components/Services/services'
-import Footer from './components/Footer/Footer'
 
 function App() {
   return (
-    <div>
-      <Navigation />
-      <Showcase />
-      <Testimonial />
-      <Services />
-      <Footer />
-    </div>
+    <div>App</div>
   );
 }
 
