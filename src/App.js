1c78e9912c9a57fb2ffec7d268e44b61e09f0bdd
Wandonium <hillarywando@gmail.com>
2023-03-01 18:04:16 +0300

    Started building the navbar inside the App component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { Stack, Avatar, Link, Typography, Box } from '@mui/material';
+import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
 
 function App() {
   return (
-    <div>App</div>
+    <Stack direction="row">
+      <Stack direction="row" spacing={1} alignItems="center">
+        <Avatar sx={{ background: "#444b48", width: "24px", height: "24px" }}>
+          {/* <LocalPhoneOutlinedIcon fontSize="small" /> */}
+          <LocalPhoneOutlinedIcon sx={{ fontSize: 16 }} />
+        </Avatar>
+        <Link href="#" color="inherit" variant="body1">(415) 523 8837</Link>
+        <Typography variant="body1">Questions? We're always open.</Typography>
+      </Stack>
+    </Stack>
   );
 }
 
