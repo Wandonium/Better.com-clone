f9eae2540756cb68e7d2470c6b95f69a347f9884
Wandonium <hillarywando@gmail.com>
2023-03-02 10:17:46 +0300

    Commit before removing use of Grid component in the App component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-import { Stack, Avatar, Link, Typography, Box } from '@mui/material';
+import { Stack, Avatar, Link, Typography, Grid } from '@mui/material';
 import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
 
 function App() {
+  const bgColor = "rgb(255, 253, 250)";
   return (
-    <Stack direction="row">
-      <Stack direction="row" spacing={1} alignItems="center">
-        <Avatar sx={{ background: "#444b48", width: "24px", height: "24px" }}>
-          {/* <LocalPhoneOutlinedIcon fontSize="small" /> */}
-          <LocalPhoneOutlinedIcon sx={{ fontSize: 16 }} />
-        </Avatar>
-        <Link href="#" color="inherit" variant="body1">(415) 523 8837</Link>
-        <Typography variant="body1">Questions? We're always open.</Typography>
-      </Stack>
-    </Stack>
+    <Grid container columns={24}>
+      <Grid item xs={1} sx={{ bgcolor: bgColor }}></Grid>
+      <Grid item xs={22} sx={{ bgcolor: bgColor }}>
+        <Stack direction="row" sx={{ height: "48px", padding: "12px 0px" }}>
+          <Stack direction="row" spacing={1} alignItems="center">
+            <Avatar sx={{ background: "#444b48", width: "24px", height: "24px" }}>
+              {/* <LocalPhoneOutlinedIcon fontSize="small" /> */}
+              <LocalPhoneOutlinedIcon sx={{ fontSize: 16 }} />
+            </Avatar>
+            <Link href="#" 
+              color="#444b48" 
+              sx={{ 
+                fontSize: 17, 
+                fontWeight: "bold", 
+                textDecoration: "underline",
+                textUnderlineOffset: "3px"
+              }}
+            >
+              (415) 523 8837
+            </Link>
+            <Typography color="#444b48" sx={{ fontSize: 15, fontWeight: "bold" }}>Questions? We're always open.</Typography>
+          </Stack>
+          <Stack direction="row" spacing={1} alignItems="center" color="#444b48" >
+            <Typography sx={{ fontWeight: "bold" }}>$100 billion</Typography>
+            <Typography>in online home loans</Typography>
+          </Stack>
+        </Stack>
+      </Grid>
+      <Grid item xs={1} sx={{ bgcolor: bgColor }}></Grid>
+    </Grid>
   );
 }
 
