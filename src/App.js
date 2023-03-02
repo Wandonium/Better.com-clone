b74fc35bac81d27a401ad82e2b902ed5a6219827
Wandonium <hillarywando@gmail.com>
2023-03-02 14:44:08 +0300

    Done building the BasicMenu component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,65 @@
-import { Stack, Avatar, Link, Typography, Grid } from '@mui/material';
+import { Stack, Avatar, Link, Typography, Box, SvgIcon } from '@mui/material';
 import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
+import { ReactComponent as Logo } from './logo.svg';
+
+import BasicMenu from './components/BasicMenu';
+
+const buyOrSellMenu = [
+  {
+    title: "Get pre-approved",
+    link: "https://better.com/start"
+  },
+  {
+    title: "Purchase rates",
+    link: "https://better.com/mortgage-rates/purchase"
+  },
+  {
+    title: "Find an Agent",
+    link: "https://better.com/with/find-an-agent"
+  },
+  {
+    title: "Mortgage Calculator",
+    link: "https://better.com/mortgage-calculator"
+  },
+  {
+    title: "Learning Center",
+    link: "https://better.com/content"
+  }
+];
 
 function App() {
-  const bgColor = "rgb(255, 253, 250)";
   return (
-    <Grid container columns={24}>
-      <Grid item xs={1} sx={{ bgcolor: bgColor }}></Grid>
-      <Grid item xs={22} sx={{ bgcolor: bgColor }}>
-        <Stack direction="row" sx={{ height: "48px", padding: "12px 0px" }}>
-          <Stack direction="row" spacing={1} alignItems="center">
-            <Avatar sx={{ background: "#444b48", width: "24px", height: "24px" }}>
-              {/* <LocalPhoneOutlinedIcon fontSize="small" /> */}
-              <LocalPhoneOutlinedIcon sx={{ fontSize: 16 }} />
-            </Avatar>
-            <Link href="#" 
-              color="#444b48" 
-              sx={{ 
-                fontSize: 17, 
-                fontWeight: "bold", 
-                textDecoration: "underline",
-                textUnderlineOffset: "3px"
-              }}
-            >
-              (415) 523 8837
-            </Link>
-            <Typography color="#444b48" sx={{ fontSize: 15, fontWeight: "bold" }}>Questions? We're always open.</Typography>
-          </Stack>
-          <Stack direction="row" spacing={1} alignItems="center" color="#444b48" >
-            <Typography sx={{ fontWeight: "bold" }}>$100 billion</Typography>
-            <Typography>in online home loans</Typography>
-          </Stack>
+    <Box sx={{ px: { xs: "27px", md: "52px" }, bgcolor: "rgb(255, 253, 250)" }}>
+      <Stack direction="row" justifyContent="space-between" sx={{ height: "48px", pt: "12px" }}>
+        <Stack direction="row" spacing={1} alignItems="center">
+          <Avatar sx={{ bgcolor: "#444b48", width: "24px", height: "24px" }}>
+            <LocalPhoneOutlinedIcon sx={{ fontSize: 16 }} />
+          </Avatar>
+          <Link href="#" 
+            color="#444b48" 
+            sx={{ 
+              fontSize: 17, 
+              fontWeight: "bold", 
+              textDecoration: "underline",
+              textUnderlineOffset: "3px"
+            }}
+          >
+            (415) 523 8837
+          </Link>
+          <Typography color="#444b48" sx={{ fontSize: 15, fontWeight: "bold" }}>Questions? We're always open.</Typography>
         </Stack>
-      </Grid>
-      <Grid item xs={1} sx={{ bgcolor: bgColor }}></Grid>
-    </Grid>
+        <Stack direction="row" spacing={1} alignItems="center" color="#444b48" >
+          <Typography sx={{ fontWeight: "bold" }}>$100 billion</Typography>
+          <Typography>in online home loans</Typography>
+        </Stack>
+      </Stack>
+      <Stack direction="row" spacing={3} alignItems="center">
+        <Link href="#">
+            <Logo />
+        </Link>
+        <BasicMenu title="Buy or Sell" links={buyOrSellMenu} />
+      </Stack>
+    </Box>
   );
 }
 
---
+++ src/components/BasicMenu.jsx
@@ -0,0 +1,117 @@
+import { useState } from 'react';
+import { 
+    Link, 
+    MenuItem, 
+    Popper, 
+    Paper, 
+    Grow, 
+    ClickAwayListener, 
+    MenuList 
+} from '@mui/material';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+
+
+export default function BasicMenu({ title, links }) {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [rotate, setRotate] = useState(false);
+  
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setRotate(true);
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setRotate(false);
+    setAnchorEl(null);
+  };
+
+  const arr = [
+    {
+        title: "Profile",
+        link: "https://google.com"
+    },
+    {
+        title: "My Account",
+        link: "https://bbc.com"
+    },
+    {
+        title: "Logout",
+        link: "https://cnn.com"
+    }
+  ];
+
+  return (
+    <div>
+      <Link
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+        underline="none"
+        color="inherit"
+        variant="subtitle1"
+        className={rotate ? "move-up" : null}
+        sx={{ 
+            px: 1,
+            pt: 0.5,
+            // fontSize: 19,
+            cursor: "pointer", 
+            display: "flex", 
+            justifyContent: "center",
+            transition: "margin-bottom .3s"
+        }}
+      >
+        {title} <ArrowDropDownIcon sx={{ 
+            fontSize: 28,
+            transition: "transform .3s" 
+        }} className={rotate ? "rotate-up" : "rotate-down"} />
+      </Link>
+      <Popper
+          open={open}
+          anchorEl={anchorEl}
+          role={undefined}
+          placement="bottom-start"
+          transition
+          disablePortal
+        >
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{
+                transformOrigin:
+                  placement === 'bottom-start' ? 'left top' : 'left bottom',
+              }}
+            >
+              <Paper elevation={3}>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList
+                    // autoFocusItem={open}
+                    id="composition-menu"
+                    aria-labelledby="composition-button"
+                    // onKeyDown={handleListKeyDown}
+                  >
+                    {
+                        links.map((item, idx) => (
+                            <MenuItem key={idx} selected={false}>
+                                <Link 
+                                    href={item.link} 
+                                    underline="none" 
+                                    color="rgb(41, 43, 41)"
+                                    variant="subtitle1"
+                                    sx={{ px: "32px", py: "10px" }}
+                                >
+                                    {item.title}
+                                </Link>
+                            </MenuItem>
+                        ))
+                    }
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
+    </div>
+  );
+}
